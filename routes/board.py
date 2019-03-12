b2d7b9663a0590dda47c8e2e5812c5f4362f6c0c
Maxiaoteng <maxiaoteng@yunfutech.com>
2019-03-12 15:05:34 +0800

    无需修改

--- routes/board.py
+++ routes/board.py
@@ -20,8 +20,6 @@ def index():
     if user.admin is False:
         return redirect(url_for('index'))
     bs = Board.find_all(deleted=False)
-    if bs = []:
-        bs = None
     return render_template('board/admin_index.html', user=user, boards=bs)
 
 
